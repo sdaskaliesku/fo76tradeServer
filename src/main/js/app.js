ce4932b0e2d818d7e9ec86578f241029c66f19c1
veronkrot <veronkrotenko9@gmail.com>
2020-08-09 15:02:58 +0300

    ui test

---
+++ src/main/js/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+      <div>Hi!</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('react'));
