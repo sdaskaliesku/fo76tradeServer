b8659c733c70943307d6a216b1855c291ee259e9
sdaskaliesku <manson.serg@gmail.com>
2021-01-31 04:26:42 +0200

    Bump website version

--- src/main/ui/domain.ts
+++ src/main/ui/domain.ts
@@ -6,7 +6,7 @@ export const DISCORD_LINK = 'https://discord.gg/7fef733';
 export const NEXUS_LINK = 'https://www.nexusmods.com/fallout76/mods/698';
 export const GH_LINK = 'https://github.com/sdaskaliesku/fo76tradeServer';
 export const COMPANION_LINK = 'https://www.nexusmods.com/fallout76/mods/744';
-export const APP_VERSION = '0.92';
+export const APP_VERSION = '0.93';
 export const MIN_MOD_SUPPORTED_VERSION = 0.4;
 
 export declare interface ModDataRequest {
